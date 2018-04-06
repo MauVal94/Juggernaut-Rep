ab99e58100f01b61e49826eb5f517c0537464c74
TyBaron <tbc35@txstate.edu>
2018-04-06 13:50:05 -0500

    Update driver.js

--- users/driver.js
+++ users/driver.js
@@ -2,7 +2,8 @@ var mongoose = require('mongoose');
 var DriverSchema = new mongoose.Schema({
     name: String,
     rating: String,
-    location: String,
+    longitude: String,
+    latitude: String,
     available: Boolean
 });
 mongoose.model('driver', DriverSchema);
