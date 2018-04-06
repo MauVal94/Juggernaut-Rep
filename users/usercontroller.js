9a1c9d36dc5495a1880a8c7dc0c8bd5d0b711844
TyBaron <tbc35@txstate.edu>
2018-04-06 13:49:23 -0500

    Update usercontroller.js

--- users/usercontroller.js
+++ users/usercontroller.js
@@ -3,15 +3,40 @@ var router = express.Router();
 var bodyParser = require('body-parser');
 router.use(bodyParser.urlencoded({ extended: true }));
 
-var Driver = require('./driver');
+var User = require('./user');
 
-//get driver from database
+//GET: get a user from database
 router.get('/:id', function (req, res) {
-    Driver.findById(req.params.id, function (err, driver) {
-        if (err) return res.status(500).send("There was a problem finding the driver.");
-        if (!driver) return res.status(404).send("No driver found.");
-        res.status(200).send(driver);
+    User.findById(req.params.id, function (err, user) {
+        if (err) return res.status(500).send("There was a problem finding the user.");
+        if (!user) return res.status(404).send("No user found.");
+        res.status(200).send(user);
     });
 });
 
+//GET: get all users from the database
+router.get('/', function (req, res) {
+    User.find({}, function (err, user) {
+        if (err) {
+            return res.status(500).send("There was a problem finding the user.");
+        }
+        res.status(200).send(user);
+    });
+});
+
+//POST:create user
+router.post('/', function (req,res){
+    User.create(req.body).then(function (user){
+        res.status(200).send(user)
+    });
+});
+
+//PUT: update user info
+//DELETE: delete a user
+router.delete('/:id', function (req, res) {
+    User.findByIdAndRemove(req.params.id, function (err, user) {
+        if (err) return res.status(500).send("There was a problem deleting the user.");
+        res.status(200).send("User: "+ user.name +" was deleted.");
+    });
+});
 module.exports = router;
