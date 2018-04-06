9da5f9fb78af4b459ee5380affc372d6a65b4b03
TyBaron <tbc35@txstate.edu>
2018-04-06 13:49:48 -0500

    Update user.js

--- users/user.js
+++ users/user.js
@@ -2,7 +2,9 @@ var mongoose = require('mongoose');
 var UserSchema = new mongoose.Schema({
     name: String,
     rating: String,
-    location: String
+    longitude: String,
+    latitude: String,
+    available: Boolean
 });
 mongoose.model('user', UserSchema);
 module.exports = mongoose.model('user');
