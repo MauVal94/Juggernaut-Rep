8ea22a7a18c008c47bbdca7db57dfd94ac1f51a3
TyBaron <tbc35@txstate.edu>
2018-04-06 13:48:48 -0500

    Update admincontroller.js

--- users/admincontroller.js
+++ users/admincontroller.js
@@ -3,38 +3,40 @@ var router = express.Router();
 var bodyParser = require('body-parser');
 router.use(bodyParser.urlencoded({ extended: true }));
 
-var Driver = require('./driver');
+var Admin = require('./admin');
 
-//create a new driver
-router.post('/', function (req,res){
-    Driver.create({
-            name: req.body.name,
-            rating: req.body.rating,
-            location: req.body.location
-
-        },
-        function (err, driver){
-            if(err){
-                return res.status(500).send("There was a problem adding the information to the database.");
-            }
-            res.status(200).send(driver);
-        });
-});
-//get driver from database
+//GET: get a admin from database
 router.get('/:id', function (req, res) {
-    Driver.findById(req.params.id, function (err, driver) {
-        if (err) return res.status(500).send("There was a problem finding the driver.");
-        if (!driver) return res.status(404).send("No driver found.");
-        res.status(200).send(driver);
+    Admin.findById(req.params.id, function (err, admin) {
+        if (err) return res.status(500).send("There was a problem finding the admin.");
+        if (!admin) return res.status(404).send("No admin found.");
+        res.status(200).send(admin);
     });
 });
 
-//  DELETES A DRIVER FROM THE DATABASE
- router.delete('/:id', function (req, res) {
-     Book.findByIdAndRemove(req.params.id, function (err, driver) {
-         if (err) return res.status(500).send("There was a problem deleting the driver.");
-         res.status(200).send("Book: "+ driver.name +" was deleted.");
-     });
- });
+//GET: get all admins from the database
+router.get('/', function (req, res) {
+    Admin.find({}, function (err, admin) {
+        if (err) {
+            return res.status(500).send("There was a problem finding the admin.");
+        }
+        res.status(200).send(admin);
+    });
+});
+
+//POST:create admin
+router.post('/', function (req,res){
+    Admin.create(req.body).then(function (admin){
+        res.status(200).send(admin)
+    });
+});
+
+//DELETE: delete a admin
+router.delete('/:id', function (req, res) {
+    Admin.findByIdAndRemove(req.params.id, function (err, admin) {
+        if (err) return res.status(500).send("There was a problem deleting the admin.");
+        res.status(200).send("admin: "+ admin.name +" was deleted.");
+    });
+});
 
 module.exports = router;
