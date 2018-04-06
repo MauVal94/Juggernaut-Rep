3353e9ed3da08ea893c24d4880b1ab55d855b26a
TyBaron <tbc35@txstate.edu>
2018-04-06 16:55:51 -0500

    Update usercontroller.js

--- users/usercontroller.js
+++ users/usercontroller.js
@@ -3,40 +3,47 @@ var router = express.Router();
 var bodyParser = require('body-parser');
 router.use(bodyParser.urlencoded({ extended: true }));
 
-var User = require('./user');
+var Admin = require('./admin');
 
-//GET: get a user from database
-router.get('/:id', function (req, res) {
-    User.findById(req.params.id, function (err, user) {
-        if (err) return res.status(500).send("There was a problem finding the user.");
-        if (!user) return res.status(404).send("No user found.");
-        res.status(200).send(user);
-    });
-});
-
-//GET: get all users from the database
-router.get('/', function (req, res) {
-    User.find({}, function (err, user) {
-        if (err) {
-            return res.status(500).send("There was a problem finding the user.");
-        }
-        res.status(200).send(user);
-    });
-});
-
-//POST:create user
+//POST:create admin
 router.post('/', function (req,res){
-    User.create(req.body).then(function (user){
-        res.status(200).send(user)
+
+    Admin.create({
+        name : req.body.name
+    },
+    function (err, admin){
+        if(err){
+            return res.status(500).send("There was a problem adding the information to the database.");
+        }
+        res.status(200).send(admin);
     });
 });
 
-//PUT: update user info
-//DELETE: delete a user
-router.delete('/:id', function (req, res) {
-    User.findByIdAndRemove(req.params.id, function (err, user) {
-        if (err) return res.status(500).send("There was a problem deleting the user.");
-        res.status(200).send("User: "+ user.name +" was deleted.");
+//GET: get a admin from database
+router.get('/:id', function (req, res) {
+    Admin.findById(req.params.id, function (err, admin) {
+        if (err) return res.status(500).send("There was a problem finding the admin.");
+        if (!admin) return res.status(404).send("No admin found.");
+        res.status(200).send(admin);
     });
 });
+
+//GET: get all admins from the database
+router.get('/', function (req, res) {
+    Admin.find({}, function (err, admin) {
+        if (err) {
+            return res.status(500).send("There was a problem finding the admin.");
+        }
+        res.status(200).send(admin);
+    });
+});
+
+//DELETE: delete a admin
+router.delete('/:id', function (req, res) {
+    Admin.findByIdAndRemove(req.params.id, function (err, admin) {
+        if (err) return res.status(500).send("There was a problem deleting the admin.");
+        res.status(200).send("admin: "+ admin.name +" was deleted.");
+    });
+});
+
 module.exports = router;
