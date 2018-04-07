2227d2e8a4b129e2f458c798fb20d73abee71ab8
TyBaron <tbc35@txstate.edu>
2018-04-06 19:19:58 -0500

    Update app.js

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ var db = require('./db');
 var driverController = require('./users/drivercontroller');
 var userController = require('./users/usercontroller');
 var adminController = require('./users/admincontroller');
+app.use(bodyParser.json());
 app.use('/drivers', driverController);
 app.use('/users', userController);
 app.use('/admin', adminController);
