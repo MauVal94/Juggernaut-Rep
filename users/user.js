0abc1f4cc07f4e210c87f074fde1dc782ca8f268
TyBaron <tbc35@txstate.edu>
2018-04-06 19:19:26 -0500

    Update user.js

--- users/user.js
+++ users/user.js
@@ -1,10 +1,25 @@
 var mongoose = require('mongoose');
 var UserSchema = new mongoose.Schema({
-    name: String,
-    rating: String,
-    longitude: String,
-    latitude: String,
-    available: Boolean
+    name: {
+        type: String,
+        required: [true, 'Name field is required']
+    },
+    rating: {
+        type: Number,
+        required: [true, 'rating field is required']
+    },
+    longitude: {
+        type: Number,
+        required: [true, 'longitude field is required']
+    },
+    latitude: {
+        type: Number,
+        required: [true, 'latitude field is required']
+    },
+    available: {
+        type: Boolean,
+        required: [true, 'rating field is required']
+    }
 });
 mongoose.model('user', UserSchema);
 module.exports = mongoose.model('user');
