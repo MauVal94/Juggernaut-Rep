4372b4f5386421a8e53084452c638dfbcbe81f08
MichaelMcBride1 <35752461+MichaelMcBride1@users.noreply.github.com>
2018-04-11 11:28:23 -0500

    Add files via upload

---
+++ googleController.js
@@ -0,0 +1,51 @@
+var express = require('express');
+var router = express.Router();
+var googleMapsClient = require('@google/maps').createClient({
+    key: 'AIzaSyCUzmUT7tJTRJtZ5KpL4Y4Mr6DmaPhSt6A',
+    Promise: Promise
+});
+
+router.post('/driverDistance', function(req, res) {
+    var location = req.body;
+    var address = location.address;
+    var city = location.city;
+    var state = location.state;
+    var fullAddress = `${address} ${city}, ${state}`;
+
+    googleMapsClient.geocode({address: fullAddress})
+        .asPromise()
+        .then((response) => {
+            var results = response.json.results;
+            var result = results[0];
+            var thisFuckingLocation = result.geometry.location;
+            var thatFuckingLocation = {
+                "lat": 40.478157,
+                "lng": -88.95118699999999
+            }
+            googleMapsClient.distanceMatrix({
+                origins: [ thisFuckingLocation ],
+                destinations: [ thatFuckingLocation ]
+            })
+                .asPromise()
+                .then(response => {
+                    // console.log('response', response.json);
+                    var duration = response.json.rows[0].elements[0].duration.text;
+                    res.send(duration);
+                })
+                .catch(err => {
+                    res.send('you done fucked up in this other function right meow');
+                    console.log(err);
+                });
+        })
+        .catch((err) => {
+            res.send('you fucked up now');
+            console.log(err);
+        });
+
+});
+
+module.exports = router;
+
+
+
+
