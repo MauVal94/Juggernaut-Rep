92e7e85065c7197f177ef69a964f4dc6067e5350
MichaelMcBride1 <35752461+MichaelMcBride1@users.noreply.github.com>
2018-04-11 11:29:47 -0500

    Add files via upload

--- app.js
+++ app.js
@@ -1,13 +1,19 @@
 var express = require('express');
 var app = express();
 var db = require('./db');
+var bodyParser = require('body-parser');
 
 var driverController = require('./users/drivercontroller');
 var userController = require('./users/usercontroller');
 var adminController = require('./users/admincontroller');
-app.use(bodyParser.json());
+var googleController = require('./googleController');
+
 app.use('/drivers', driverController);
 app.use('/users', userController);
 app.use('/admin', adminController);
+app.use(bodyParser.json()); // for parsing application/json
+app.use('/google', googleController);
 
 module.exports = app;
+
+//require('./GoogleHandler/google');
