acd464310dae711c64e9299b65fe8e88eae0bf09
TyBaron <tbc35@txstate.edu>
2018-04-17 10:28:15 -0500

    Now finds estimated time between a user and a driver using id

--- googleController.js
+++ googleController.js
@@ -5,47 +5,55 @@ var googleMapsClient = require('@google/maps').createClient({
     Promise: Promise
 });
 
-router.post('/driverDistance', function(req, res) {
-    var location = req.body;
-    var address = location.address;
-    var city = location.city;
-    var state = location.state;
-    var fullAddress = `${address} ${city}, ${state}`;
+var User = require('./users/user');
+var Driver = require('./users/driver');
 
-    googleMapsClient.geocode({address: fullAddress})
+router.get('/distance', function(req, res) {
+    var startLat = 30.0;
+    var startLng = -97.941394;
+    var endLat = 29.883275;
+    var endLng = -97.941394;
+
+    var start = User.findById(req.query.id, function start(err, user) {
+        if (err) return res.status(500).send("There was a problem finding the user.");
+        if (!user) return res.status(404).send("No user found.");
+        console.log(user.name);
+    })
+
+    var end = Driver.findById(req.query.id, function end(err, driver) {
+        if (err) return res.status(500).send("There was a problem finding the driver.");
+        if (!driver) return res.status(404).send("No user found.");
+        Driver = driver;
+        console.log(Driver.name+" sdf");
+    })
+    console.log(endLng);
+    console.log(start.name);
+    console.log(end.name);
+
+    var thisLocation = {
+        "lat": startLat,
+        "lng": startLng
+    }
+    var thatLocation = {
+        "lat": endLat,
+        "lng": endLng
+    }
+    googleMapsClient.distanceMatrix({
+        origins: [ thisLocation ],
+        destinations: [ thatLocation ]
+    })
         .asPromise()
-        .then((response) => {
-            var results = response.json.results;
-            var result = results[0];
-            var thisLocation = result.geometry.location;
-            var thatLocation = {
-                "lat": 40.478157,
-                "lng": -88.95118699999999
-            }
-            googleMapsClient.distanceMatrix({
-                origins: [ thisLocation ],
-                destinations: [ thatLocation ]
+        .then(response => {
+            // console.log('response', response.json);
+            var duration = response.json.rows[0].elements[0].duration.text;
+            var distance = response.json.rows[0].elements[0].distance.text;
+            res.send(duration+" "+distance);
             })
-                .asPromise()
-                .then(response => {
-                    // console.log('response', response.json);
-                    var duration = response.json.rows[0].elements[0].duration.text;
-                    res.send(duration);
-                })
-                .catch(err => {
-                    res.send('There was a problem with your function');
-                    console.log(err);
-                });
-        })
-        .catch((err) => {
-            res.send('ERROR');
+        .catch(err => {
+            res.send('There was a problem with your function');
             console.log(err);
-        });
+            });
 
 });
 
 module.exports = router;
-
-
-
-
