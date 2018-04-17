b0a4385abbdcf32ba04279394323bc024497fcd1
TyBaron <tbc35@txstate.edu>
2018-04-17 17:51:36 -0500

    Update googleController.js

--- googleController.js
+++ googleController.js
@@ -1,59 +1,75 @@
 var express = require('express');
 var router = express.Router();
+const http = require('http');
 var googleMapsClient = require('@google/maps').createClient({
     key: 'AIzaSyCUzmUT7tJTRJtZ5KpL4Y4Mr6DmaPhSt6A',
     Promise: Promise
 });
 
-var User = require('./users/user');
-var Driver = require('./users/driver');
 
 router.get('/distance', function(req, res) {
-    var startLat = 30.0;
-    var startLng = -97.941394;
-    var endLat = 29.883275;
-    var endLng = -97.941394;
 
-    var start = User.findById(req.query.id, function start(err, user) {
-        if (err) return res.status(500).send("There was a problem finding the user.");
-        if (!user) return res.status(404).send("No user found.");
-        console.log(user.name);
-    })
+    var users = req.query.id;
+    var drivers = req.query.id2;
+    var userRequest = ("http://localhost:3000/users/"+users);
+    var driverRequest = ("http://localhost:3000/drivers/"+drivers);
 
-    var end = Driver.findById(req.query.id, function end(err, driver) {
-        if (err) return res.status(500).send("There was a problem finding the driver.");
-        if (!driver) return res.status(404).send("No user found.");
-        Driver = driver;
-        console.log(Driver.name+" sdf");
-    })
-    console.log(endLng);
-    console.log(start.name);
-    console.log(end.name);
 
-    var thisLocation = {
-        "lat": startLat,
-        "lng": startLng
-    }
-    var thatLocation = {
-        "lat": endLat,
-        "lng": endLng
-    }
-    googleMapsClient.distanceMatrix({
-        origins: [ thisLocation ],
-        destinations: [ thatLocation ]
-    })
-        .asPromise()
-        .then(response => {
-            // console.log('response', response.json);
-            var duration = response.json.rows[0].elements[0].duration.text;
-            var distance = response.json.rows[0].elements[0].distance.text;
-            res.send(duration+" "+distance);
-            })
-        .catch(err => {
-            res.send('There was a problem with your function');
-            console.log(err);
+    var thatLocation;
+    var thisLocation;
+    var origin = (function (callback){
+        http.get(userRequest, (res) =>{
+            let data = '';
+            res.on('data', (chunk) => {
+                data += chunk;
             });
+            res.on('end', () => {
+                thisLocation = {
+                    "lat": (JSON.parse(data).latitude),
+                    "lng": (JSON.parse(data).longitude)
+                };
+                callback(null, thisLocation);
+            })
+        });
+    });
 
-});
+    var destination = (function (callback) {
+        http.get(driverRequest, (res) => {
+            let data = '';
+            res.on('data', (chunk) => {
+                data += chunk;
+            });
+            res.on('end', () => {
+                thatLocation = {
+                    "lat": (JSON.parse(data).latitude),
+                    "lng": (JSON.parse(data).longitude)
+                };
+                callback(null, thatLocation);
+            })
+        });
+    });
+    origin(function (err, data) {
+        thisLocation = data;
+        destination(function (err, data) {
+            thatLocation = data;
+            googleMapsClient.distanceMatrix({
+                origins: [thisLocation],
+                destinations: [ thatLocation ]
+
+            })
+                .asPromise()
+                .then(response => {
+                    var duration = response.json.rows[0].elements[0].duration.text;
+                    var distance = response.json.rows[0].elements[0].distance.text;
+                    res.send(duration+" "+distance);
+                })
+                .catch(err => {
+                    res.send('There was a problem with your function');
+                    console.log(err);
+                });
+
+        });
+        })
+    });
 
 module.exports = router;
