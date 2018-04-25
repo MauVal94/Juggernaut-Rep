9fca0ec73ba7a4c22c14675c037014d235990521
TyBaron <tbc35@txstate.edu>
2018-04-25 17:29:59 -0500

    Update db.js

--- db.js
+++ db.js
@@ -1,7 +1,7 @@
 var mongoose = require('mongoose');
 
 mongoose.useMongoClient
-mongoose.connect('mongodb://admin:admin@juggernaut-cluster-shard-00-00-pam8k.mongodb.net:27017,juggernaut-cluster-shard-00-01-pam8k.mongodb.net:27017,juggernaut-cluster-shard-00-02-pam8k.mongodb.net:27017/test?ssl=true&replicaSet=Juggernaut-Cluster-shard-0&authSource=admin')
+mongoose.connect('mongodb://admin:admin@juggernaut-cluster-shard-00-00-pam8k.mongodb.net:27017,juggernaut-cluster-shard-00-01-pam8k.mongodb.net:27017,juggernaut-cluster-shard-00-02-pam8k.mongodb.net:27017/nuber?ssl=true&replicaSet=Juggernaut-Cluster-shard-0&authSource=admin')
     .then(function (data){
     console.log(data);
 }).catch(function (error) {
