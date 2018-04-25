108f1b0570a997fbc680856d64ada8fe21ea3e92
TyBaron <tbc35@txstate.edu>
2018-04-25 17:07:54 -0500

    Update googleController.js

--- googleController.js
+++ googleController.js
@@ -7,102 +7,85 @@ var googleMapsClient = require('@google/maps').createClient({
 });
 
 var Drivers = require('./users/driver');
+var Users = require('./users/user');
 
 router.get('/distance', function(req, res) {
 
-    var users = req.query.userID;
-    var drivers = req.query.driverID;
-    var userRequest = ("http://localhost:3000/users/"+users);
-    var driverRequest = ("http://localhost:3000/drivers/"+drivers);
+    var usersID = req.query.userID;
+    var driversID = req.query.driverID;
 
 
     var thatLocation;
     var thisLocation;
-    var origin = (function (callback){
-        http.get(userRequest, (res) =>{
-            let data = '';
-            res.on('data', (chunk) => {
-                data += chunk;
-            });
-            res.on('end', () => {
-                thisLocation = {
-                    "lat": (JSON.parse(data).latitude),
-                    "lng": (JSON.parse(data).longitude)
-                };
-                callback(null, thisLocation);
-            })
+    var origin = (function (callback) {
+        Users.findById(usersID, function (err, user) {
+            thisLocation = {
+                "lat": (JSON.stringify(user.latitude)),
+                "lng": (JSON.stringify(user.longitude))
+            };
+            callback(null, thisLocation);
         });
-    });
+    })
 
     var destination = (function (callback) {
-        http.get(driverRequest, (res) => {
-            let data = '';
-            res.on('data', (chunk) => {
-                data += chunk;
-            });
-            res.on('end', () => {
-                thatLocation = {
-                    "lat": (JSON.parse(data).latitude),
-                    "lng": (JSON.parse(data).longitude)
-                };
-                callback(null, thatLocation);
-            })
+        Drivers.findById(driversID, function (err, driver) {
+            thatLocation = {
+                "lat": (JSON.stringify(driver.latitude)),
+                "lng": (JSON.stringify(driver.longitude))
+            };
+            callback(null, thatLocation);
         });
-    });
+    })
     origin(function (err, data) {
         thisLocation = data;
         destination(function (err, data) {
             thatLocation = data;
             googleMapsClient.distanceMatrix({
-                    origins: [thisLocation],
-                    destinations: [thatLocation]
-                },
-                function (response) {
+                origins: [thisLocation],
+                destinations: [ thatLocation ]
+
+            })
+                .asPromise()
+                .then(response => {
                     var duration = response.json.rows[0].elements[0].duration.text;
-                    var distance = response.json.rows[0].elements[0].distance.text;
-                    res.send(duration + " " + distance);
+                    res.send(duration);
+                })
+                .catch(err => {
+                    res.send('There was a problem with your function');
+                    console.log(err);
                 });
+
         });
     })
 });
 
 router.get('/proximity', function(req, res) {
-    var users = req.query.userID;
+    var usersID = req.query.userID;
     var proximity = req.query.proximity;
     var thisLocation;
     var thatLocation;
-    var userRequest = ("http://localhost:3000/users/"+users);
-    var driverCount;
 
     var origin = (function (callback) {
-        http.get(userRequest, (res) => {
-            let data = '';
-            res.on('data', (chunk) => {
-                data += chunk;
-            });
-            res.on('end', () => {
-                thisLocation = {
-                    "lat": (JSON.parse(data).latitude),
-                    "lng": (JSON.parse(data).longitude)
-                };
-                callback(null, thisLocation);
-            })
+        Users.findById(usersID, function (err, user) {
+            thisLocation = {
+                "lat": (JSON.stringify(user.latitude)),
+                "lng": (JSON.stringify(user.longitude))
+            };
+            callback(null, thisLocation);
         });
-    });
+    })
 
-    Drivers.count(function (err, count) {
-        if (err) return res.status(500).send("There was a problem finding the driver.");
-        driverCount = count;
 
-        origin(function (err, data) {
-            thisLocation = data;
+    origin(function (err, data) {
+        thisLocation = data;
 
-            Drivers.find({}, function (err, drivers) {
-                if (err) {
-                    return res.status(500).send("There was a problem finding the driver.");
-                }
+        Drivers.find({}, function (err, drivers) {
+            if (err) {
+                return res.status(500).send("There was a problem finding the driver.");
+            }
 
-                let acceptedDrivers = drivers.filter(function(driver){
+            let acceptedDriver =   drivers.filter(async function (driver) {
+                var isAvailable =  await new Promise(function(resolve, reject){
                     thatLocation = {
                         "lat": JSON.stringify(driver.latitude),
                         "lng": JSON.stringify(driver.longitude)
@@ -112,20 +95,22 @@ router.get('/proximity', function(req, res) {
                         origins: [thisLocation],
                         destinations: [thatLocation]
 
-                    }, function(response) {
-                            let distance = response.json.rows[0].elements[0].distance.value / 1000;
-                            console.log(distance);
-                            if (distance < proximity) {
-                                return driver;
-                            }
-                        })
-                        .catch(err => {
-                            res.send('There was a problem with your function');
-                            console.log(err);
-                        })
+                    }, function (err, result) {
+                        let distance = result.json.rows[0].elements[0].distance.value / 1000;
+                        console.log(distance);
+                        let available = JSON.stringify(driver.available);
+                        if (distance < proximity && available) {
+                            resolve(isAvailable = true);
+                        }
+                        else
+                           resolve(isAvailable = false);
+                    });
                 });
-                res.send(acceptedDrivers);
+                console.log(isAvailable);
+                return isAvailable;
             });
+            console.log(acceptedDriver);
+            res.send(acceptedDriver)
         });
     });
 });
